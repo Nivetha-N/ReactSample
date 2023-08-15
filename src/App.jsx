1379736db7c8ca33c2961f2ab1b58a5e779b806f
Nivetha-N <nivenagaraj123@gmail.com>
2023-08-15 12:32:55 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>React Day3_task-Pricecards</h1>
+      <br/>
+    </>
+  )
+}
+
+export default App
